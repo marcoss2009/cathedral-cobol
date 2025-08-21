b0a47e79b6a94ef455f0a3ea12d25c46a5903ed7
Marcos Muñoz <mmunoz@americanbreakfast.com.ar>
2025-08-21 07:46:41 -0300

    compile.sh

---
+++ compile.sh
@@ -0,0 +1,8 @@
+cobc -c alta.cbl
+cobc -c consultar.cbl
+cobc -c movimientos.cbl
+cobc -c caja.cbl
+cobc -c buscar.cbl
+cobc -c fecha.cbl
+cobc -c movimiento.cbl
+cobc -x cathedral.cbl alta.o consultar.o movimientos.o caja.o buscar.o fecha.o movimiento.o
